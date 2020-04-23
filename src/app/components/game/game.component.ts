08435f52c72893a7fedf5a37f2a09c12ccb339d0
Tomasz_Trzcinski <Tomasz_Trzcinski>
2020-04-23 23:49:54 +0200

    Added check of http client wit exchange rates.

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {CurrencyClientService, RootObject} from '../../services/currency-client.service';
 
 @Component({
   selector: 'app-game',
@@ -9,11 +10,16 @@ export class GameComponent implements OnInit {
 
   messageForUser: string;
   rolled: number;
+  rootObject: RootObject;
+  result: string;
 
-  constructor() {
+  constructor(private currencyClientService: CurrencyClientService) {
   }
 
   ngOnInit(): void {
+    this.currencyClientService.getCurrency().subscribe(value => {
+      this.rootObject = value;
+    });
   }
 
   private rollMe(diceSize: number) {
@@ -41,6 +47,16 @@ export class GameComponent implements OnInit {
       case 6:
         this.messageForUser = 'Hallo ' + value + '!';
         break;
+    }
+  }
+
+  check(value: number) {
+    if (value > this.rootObject.rates.PLN) {
+      this.result = 'Your value is too high.';
+    } else if (value < this.rootObject.rates.PLN) {
+      this.result = 'Your value is too low.';
+    } else {
+      this.result = 'Congratulations! You guested it.';
     }
   }
 }
